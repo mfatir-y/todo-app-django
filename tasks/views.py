23a28fe4fff8060971d18f132871f8c5fd657a2d
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-07 20:01:38 -0400

    Creating item using POST method from lists.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models, forms
 
 def index(request):
     tasks = models.Task.objects.all()
     form = forms.TaskForm()
+
+    if request.method == 'POST':
+        form = forms.TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     return render(request, 'tasks/lists.html', {'tasks': tasks, 'form': form})
