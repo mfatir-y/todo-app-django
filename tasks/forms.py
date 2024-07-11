b26cc15d8e6db52aefc6085ceceada1dbbba0c6c
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-11 14:29:57 -0400

    Styled the main page and used django to add styles to form input elements

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from . import models
 
 class TaskForm(ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task', 'style': "padding: 0.3rem 1rem; border-radius: 0.5rem;"}))
 
     class Meta:
         model = models.Task
