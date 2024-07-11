680a9d462e4345bf9f191bfe8705a3422ef6d7f6
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-11 14:37:06 -0400

    Styled the header

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from . import models
 
 class TaskForm(ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task', 'style': "padding: 0.3rem 1rem; border-radius: 0.5rem;"}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task', 'style': "padding: 0.3rem 1rem; border-radius: 0.5rem; margin: 1rem 0;"}))
 
     class Meta:
         model = models.Task
