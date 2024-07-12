4c17369a9aee6b356c687c5def6083a3db5b98df
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-12 19:34:42 -0400

    Styled update_task page

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,7 +3,9 @@ from django.forms import ModelForm
 from . import models
 
 class TaskForm(ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task', 'style': "padding: 0.3rem 1rem; border-radius: 0.5rem; margin: 1rem 0;"}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task',
+                                                           'style': "padding: 0.3rem 1rem; border-radius: 0.5rem; margin: 1rem 0;"}))
+    complete = forms.BooleanField(widget=forms.CheckboxInput(attrs={'style': "margin: 0; padding: 0; display: inline-block; width: 1rem;"}))
 
     class Meta:
         model = models.Task
